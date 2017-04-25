538d2b9f7b8c7ac8dc693f3eb66106b8f418c301
Pablo Ugas <pablo.ugas@gmail.com>
2017-04-25 10:43:50 -0700

    fixed font import for icons

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ Bundler.require(*Rails.groups)
 
 module ClarityRor
   class Application < Rails::Application
-      config.assets.paths << Rails.root.join("app", "assets", "fonts")
+    #   config.assets.paths << Rails.root.join("app", "assets", "fonts")
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers

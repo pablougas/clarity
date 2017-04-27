3cb2a39d676a38caccd52a1c8baf800c522c8de0
Pablo Ugas <pablo.ugas@gmail.com>
2017-04-27 16:12:25 -0700

    wip

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,42 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :prepare_meta_tags, if: "request.get?"
+
+  def prepare_meta_tags(options={})
+    site_name   = "Clarity Claims"
+    title       = [controller_name, action_name].join(" ")
+    description = "Clarity Insurance Services - "
+    image       = options[:image] || "assets/clarity.png"
+    current_url = request.url
+
+    # Let's prepare a nice set of defaults
+    defaults = {
+      site:        site_name,
+      title:       title,
+      image:       image,
+      description: description,
+      keywords:    %w[insurance claims adjusting processing legal consulting],
+      twitter: {
+        site_name: site_name,
+        site: 'Clarity',
+        card: 'summary',
+        description: description,
+        image: image
+      },
+      og: {
+        url: current_url,
+        site_name: site_name,
+        title: title,
+        image: image,
+        description: description,
+        type: 'website'
+      }
+    }
+
+    options.reverse_merge!(defaults)
+
+    set_meta_tags options
+  end
+
 end
